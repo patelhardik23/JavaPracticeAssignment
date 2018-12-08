e395d2e38cfa0528f863f9c579623c1f9aa4e34d
hardikpatel <hardikpatel@hardikpatel>
2018-12-08 20:48:58 +0530

    Updated GUI

--- PracticeAssignment/src/ausAssignment/assignment3/ProcessFoodGUI.java
+++ PracticeAssignment/src/ausAssignment/assignment3/ProcessFoodGUI.java
@@ -1,235 +1,235 @@
 package ausAssignment.assignment3;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProcessFoodGUI implements ActionListener
-{
-    static List<ProcessedFood> foodList = new ArrayList<ProcessedFood>();
-    static List<Nutrient> nutrientList = new ArrayList<Nutrient>();
+public class ProcessFoodGUI implements ActionListener {
+	
+	private static final Insets WEST_INSETS = new Insets(10, 0, 5, 15);
+	private static final Insets EAST_INSETS = new Insets(10, 20, 5, 0);
+	
+	static List<ProcessedFood> foodList = new ArrayList<ProcessedFood>();
+	static List<Nutrient> nutrientList = new ArrayList<Nutrient>();
 
-    private JFrame f;
-    private JPanel topPanel;
-    private JTextField userNameTxt;
-    private JLabel userNameLbl;
-    private JLabel cerealsLbl;
-    private JComboBox<String> cerealsComboBox;
-    private JComboBox<String> beveragesComboBox;
-    private JLabel beveragesLbl;
+	private JFrame f;
+	private JPanel topPanel;
+	private JTextField userNameTxt;
+	private JLabel userNameLbl;
+	private JLabel cerealsLbl;
+	private JComboBox<String> cerealsComboBox;
+	private JComboBox<String> beveragesComboBox;
+	private JLabel beveragesLbl;
 
-    private JPanel middlePanel;
-    private JTextArea textArea;
+	private JPanel middlePanel;
+	private JTextArea textArea;
 
-    private JPanel bottomPanel;
-    private JLabel commandLbl;
-    private JButton enterDataBtn;
-    private JButton displayChoiceBtn;
-    private JButton clearDisplayBtn;
-    private JButton quitBtn;
+	private JPanel bottomPanel;
+	private JLabel commandLbl;
+	private JButton enterDataBtn;
+	private JButton displayChoiceBtn;
+	private JButton clearDisplayBtn;
+	private JButton quitBtn;
 
-    public void intializeGUI()
-    {
-        f = new JFrame("Panel Example");
+	public void intializeGUI() {
+		f = new JFrame("Panel Example");
 
-        setTopPanel();
-        setMiddlePanel();
-        setBottomPanel();
+		setTopPanel();
+		setMiddlePanel();
+		setBottomPanel();
 
-        f.add(topPanel);
-        f.add(middlePanel);
-        f.add(bottomPanel);
+		f.add(topPanel);
+		f.add(middlePanel);
+		f.add(bottomPanel);
 
-        f.setSize(610, 600);
-        // f.setLayout(new FlowLayout(FlowLayout.RIGHT));
-        f.setLayout(null);
-        f.setVisible(true);
-        f.setResizable(false);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+		f.setSize(500, 500);
+		f.setLayout(new BoxLayout(f.getContentPane(), BoxLayout.Y_AXIS));
+		f.setVisible(true);
+		f.setResizable(false);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.pack();
+	}
 
-    private void setTopPanel()
-    {
-        topPanel = new JPanel();
+	private void setTopPanel() {
+		topPanel = new JPanel();
+		
+		topPanel.setLayout(new GridBagLayout());
+		Border border = BorderFactory.createLineBorder(new Color(0, 0, 182, 15), 8, true);
+		topPanel.setBorder(border);
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		userNameLbl = new JLabel("User Name");
+		gbc = createGbc(0, 0, 1);
+		topPanel.add(userNameLbl,gbc);
+		
+		userNameTxt = new JTextField(10);
+		gbc = createGbc(1, 0, 3);
+		userNameTxt.setEditable(true);
+		userNameLbl.setLabelFor(userNameTxt);
+		topPanel.add(userNameTxt,gbc);
+		
+		cerealsLbl = new JLabel("cereals");
+		gbc = createGbc(0, 1, 1);
+		topPanel.add(cerealsLbl,gbc);
+        
+		String country[] = { "India", "Aus", "U.S.A", "England", "Newzealand" };
 
-        topPanel.setLayout(new FlowLayout(FlowLayout.TRAILING));
+		cerealsComboBox = new JComboBox<String>(country);
+		gbc = createGbc(1, 1, 1);
+		cerealsLbl.setLabelFor(cerealsComboBox);
+		cerealsComboBox.setSelectedItem(null);
+		topPanel.add(cerealsComboBox,gbc);
 
-        topPanel.setBounds(0, 0, 600, 100);
+		beveragesLbl = new JLabel("beverages");
+		gbc = createGbc(2, 1, 1);
+		topPanel.add(beveragesLbl,gbc);
+		
+		String state[] = { "Gujarat", "Rajasthan", "M.P.", "Mumbai", "JnK" };
+		beveragesComboBox = new JComboBox<String>(state);
+		gbc = createGbc(3, 1, 1);
+		beveragesLbl.setLabelFor(beveragesComboBox);
+		beveragesComboBox.setSelectedItem(null);
+		topPanel.add(beveragesComboBox,gbc);
 
-        userNameTxt = new JTextField(45);
-        userNameTxt.setEditable(true);
+	}
+	
+	private GridBagConstraints createGbc(int x, int y, int gridwidth) {
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = x;
+		gbc.gridy = y;
+		gbc.gridwidth = gridwidth;
+		gbc.gridheight = 1;
+		gbc.anchor = (x == 0 || x == 2) ? GridBagConstraints.EAST:GridBagConstraints.WEST ;
+		gbc.fill = (x == 2 && y == 1)?GridBagConstraints.FIRST_LINE_START:GridBagConstraints.HORIZONTAL;
+		gbc.insets = (x == 0 || x == 2) ? EAST_INSETS:WEST_INSETS;
+		gbc.weightx = (x == 0) ? 0.1 : 1.0;
+		gbc.weighty = (x == 0) ? 0.1 : 1.0;
+		return gbc;
+	}
 
-        userNameLbl = new JLabel("User Name");
-        userNameLbl.setLabelFor(userNameTxt);
+	private void setMiddlePanel() {
+		middlePanel = new JPanel();
+		middlePanel.setLayout(new BoxLayout(middlePanel, BoxLayout.X_AXIS));
+		Border border = BorderFactory.createLineBorder(new Color(0, 0, 182, 15), 8, true);
+		middlePanel.setBorder(border);
+		String testData = "TestData TestData TestData TestData TestData TestData TestData TestData TestData TestData TestData TestData ";
+		textArea = new JTextArea(testData, 20, 20);
+		textArea.setMargin(new Insets(10, 10, 10, 10));
+		JScrollPane textAreaScroll = new JScrollPane(textArea);
+		middlePanel.add(textAreaScroll);
 
-        String country[] = {"India", "Aus", "U.S.A", "England", "Newzealand"};
+	}
 
-        cerealsComboBox = new JComboBox<String>(country);
-        cerealsLbl = new JLabel("cereals");
-        cerealsLbl.setLabelFor(cerealsComboBox);
-        cerealsComboBox.setBounds(10, 30, 10, 10);
-        cerealsComboBox.setSelectedItem(null);
+	private void setBottomPanel() {
+		bottomPanel = new JPanel();
 
-        String state[] = {"Gujarat", "Rajasthan", "M.P.", "Mumbai", "JnK"};
-        beveragesComboBox = new JComboBox<String>(state);
-        beveragesLbl = new JLabel("beverages");
-        beveragesLbl.setLabelFor(beveragesComboBox);
-        beveragesComboBox.setBounds(50, 30, 10, 10);
-        beveragesComboBox.setSelectedItem(null);
+		bottomPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 10));
+		Border border = BorderFactory.createLineBorder(new Color(0, 0, 182, 15), 8, true);
+		bottomPanel.setBorder(border);
 
-        topPanel.add(userNameLbl);
-        topPanel.add(userNameTxt);
-        topPanel.add(cerealsLbl);
+		commandLbl = new JLabel("Command Buttons");
+		commandLbl.setLabelFor(bottomPanel);
 
-        topPanel.add(cerealsComboBox);
+		enterDataBtn = new JButton("Enter Data");
 
-        topPanel.add(beveragesLbl);
-        topPanel.add(beveragesComboBox);
+		displayChoiceBtn = new JButton("Display Choices");
+		clearDisplayBtn = new JButton("Clear Display");
+		quitBtn = new JButton("Quit");
 
-    }
+		enterDataBtn.addActionListener(this::actionPerformed);
+		clearDisplayBtn.addActionListener(this::actionPerformed);
+		quitBtn.addActionListener(this::actionPerformed);
 
-    private void setMiddlePanel()
-    {
-        middlePanel = new JPanel();
-        middlePanel.setBackground(Color.gray);
-        middlePanel.setBounds(0, 100, 600, 400);
+		bottomPanel.add(commandLbl);
+		bottomPanel.add(enterDataBtn);
+		bottomPanel.add(displayChoiceBtn);
+		bottomPanel.add(clearDisplayBtn);
+		bottomPanel.add(quitBtn);
+	}
 
-        textArea = new JTextArea("Type here...", 26, 53);
-        // TA1.setBounds(10,210,600,200);
-        middlePanel.add(textArea);
+	private void enterDataBtnClickedAction() {
 
-    }
+		String userName = userNameTxt.getText().trim();
+		String cerealsValue = String.valueOf(cerealsComboBox.getSelectedItem()).trim();
+		String beveragesValue = String.valueOf(beveragesComboBox.getSelectedItem()).trim();
 
-    private void setBottomPanel()
-    {
-        bottomPanel = new JPanel();
-        bottomPanel.setBounds(0, 500, 600, 100);
-        bottomPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		String errorMsg = "";
+		boolean validation = false;
 
-        commandLbl = new JLabel("Command Buttons");
-        commandLbl.setLabelFor(bottomPanel);
+		if (userName.equals(null) || userName.equals("")) {
+			errorMsg = "Please Enter User Name!!!";
+			validation = false;
+		} else if (cerealsValue.equals(null) || cerealsValue.equals("null")) {
+			errorMsg = "Please select Cereals!!!";
+			validation = false;
+		} else if (beveragesValue.equals(null) || beveragesValue.equals("null")) {
+			errorMsg = "Please Enter beverages!!!";
+			validation = false;
+		} else {
+			validation = true;
+			JOptionPane optionPane = new JOptionPane("Data Inserted Successfully!!", JOptionPane.ERROR_MESSAGE);
+			JDialog dialog = optionPane.createDialog("Conngratulation..");
+			dialog.setAlwaysOnTop(true);
+			dialog.setVisible(true);
+		}
 
-        enterDataBtn = new JButton("Enter Data");
-        displayChoiceBtn = new JButton("Display Choices");
-        clearDisplayBtn = new JButton("Clear Display");
-        quitBtn = new JButton("Quit");
+		if (validation == false) {
+			JOptionPane optionPane = new JOptionPane(errorMsg, JOptionPane.ERROR_MESSAGE);
+			JDialog dialog = optionPane.createDialog("Data Validation");
+			dialog.setAlwaysOnTop(true);
+			dialog.setVisible(true);
+		}
 
-        enterDataBtn.addActionListener(this::actionPerformed);
-        clearDisplayBtn.addActionListener(this::actionPerformed);
-        quitBtn.addActionListener(this::actionPerformed);
+		if (validation == true) {
 
-        bottomPanel.add(commandLbl);
-        bottomPanel.add(enterDataBtn);
-        bottomPanel.add(displayChoiceBtn);
-        bottomPanel.add(clearDisplayBtn);
-        bottomPanel.add(quitBtn);
-    }
+			System.out.println(
+					"userName : " + userName + "::: Cereals :::" + cerealsValue + ":::Beverages :::" + beveragesValue);
+		}
+	}
 
-    private void enterDataBtnClickedAction()
-    {
-        // TA1.setText(null);
-        // userNameField.setText(null);
-        // userNameField.setEditable(false);
-        String userName = userNameTxt.getText().trim();
-        String cerealsValue = String.valueOf(cerealsComboBox.getSelectedItem()).trim();
-        String beveragesValue = String.valueOf(beveragesComboBox.getSelectedItem()).trim();
+	ProcessFoodGUI() {
+		// String userName;
+	}
 
-        String errorMsg = "";
-        boolean validation = false;
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == quitBtn) {
+			System.exit(0);
+		} else if (e.getSource() == clearDisplayBtn) {
+			textArea.setText(null);
+			userNameTxt.setText(null);
+			userNameTxt.setEditable(true);
+			cerealsComboBox.setSelectedItem(null);
+			beveragesComboBox.setSelectedItem(null);
+		} else if (e.getSource() == enterDataBtn) {
+			enterDataBtnClickedAction();
+		} else if (e.getSource() == displayChoiceBtn) {
 
-        if (userName.equals(null) || userName.equals(""))
-        {
-            errorMsg = "Please Enter User Name!!!";
-            validation = false;
-        }
-        else if (cerealsValue.equals(null) || cerealsValue.equals("null"))
-        {
-            errorMsg = "Please select Cereals!!!";
-            validation = false;
-        }
-        else if (beveragesValue.equals(null) || beveragesValue.equals("null"))
-        {
-            errorMsg = "Please Enter beverages!!!";
-            validation = false;
-        }
-        else
-        {
-            validation = true;
-            JOptionPane optionPane = new JOptionPane("Data Inserted Successfully!!", JOptionPane.ERROR_MESSAGE);
-            JDialog dialog = optionPane.createDialog("Conngratulation..");
-            dialog.setAlwaysOnTop(true);
-            dialog.setVisible(true);
-        }
+		}
+	}
 
-        if (validation == false)
-        {
-            JOptionPane optionPane = new JOptionPane(errorMsg, JOptionPane.ERROR_MESSAGE);
-            JDialog dialog = optionPane.createDialog("Data Validation");
-            dialog.setAlwaysOnTop(true);
-            dialog.setVisible(true);
-        }
+	public static void main(String args[]) {
 
-        if (validation == true)
-        {
+		String fileName = "ausAssignment3_data.csv";
+		new DataFile(fileName);
 
-            System.out.println("userName : " + userName + "::: Cereals :::" + cerealsValue + ":::Beverages :::"
-                                       + beveragesValue);
-        }
-    }
+		System.out.println("list created");
+		for (ProcessedFood pf : foodList) {
+			System.out.println(pf);
+			System.out.println("done");
+		}
+		for (Nutrient nl : nutrientList) {
+			System.out.println(nl);
+			System.out.println("doneok");
+		}
+		ProcessFoodGUI processFoodGUI = new ProcessFoodGUI();
+		processFoodGUI.intializeGUI();
 
-    ProcessFoodGUI()
-    {
-        // String userName;
-    }
-
-
-    @Override
-    public void actionPerformed(ActionEvent e)
-    {
-        if (e.getSource() == quitBtn)
-        {
-            System.exit(0);
-        }
-        else if (e.getSource() == clearDisplayBtn)
-        {
-            textArea.setText(null);
-            userNameTxt.setText(null);
-            userNameTxt.setEditable(true);
-            cerealsComboBox.setSelectedItem(null);
-            beveragesComboBox.setSelectedItem(null);
-        }
-        else if (e.getSource() == enterDataBtn)
-        {
-            enterDataBtnClickedAction();
-        }
-        else if (e.getSource() == displayChoiceBtn)
-        {
-
-        }
-    }
-
-    public static void main(String args[])
-    {
-
-        String fileName = "ausAssignment3_data.csv";
-        new DataFile(fileName);
-
-        System.out.println("list created");
-        for (ProcessedFood pf : foodList)
-        {
-            System.out.println(pf);
-            System.out.println("done");
-        }
-        for (Nutrient nl : nutrientList)
-        {
-            System.out.println(nl);
-            System.out.println("doneok");
-        }
-        ProcessFoodGUI processFoodGUI = new ProcessFoodGUI();
-        processFoodGUI.intializeGUI();
-
-    }
+	}
 }
